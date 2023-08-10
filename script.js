e2bd0ef3f1cd52b2e450ca389fef79e04a962930
Matt Christian <matthew.bryan.christian@gmail.com>
2023-08-10 16:54:09 -0400

    modular code

--- script.js
+++ script.js
@@ -1,37 +1,65 @@
-window.onload = function () {
-    bootlint.showLintReportForCurrentDocument([], {
-        hasProblems: false,
-        problemFree: false
-    });
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
-    $('[data-toggle="tooltip"]').tooltip();
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-    function formatDate(date) {
-        return (
-            date.getDate() +
-            "/" +
-            (date.getMonth() + 1) +
-            "/" +
-            date.getFullYear()
-        );
+const balls = [];
+
+class Ball {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.size = Math.random() * 30 + 5;
+        this.speedX = Math.random() * 3 - 1.5;
+        this.speedY = Math.random() * 3 - 1.5;
+        this.color = `rgb(${Math.random()*255}, ${Math.random()*255}, ${Math.random()*255})`;
     }
 
-    var currentDate = formatDate(new Date());
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
 
-    $(".due-date-button").datepicker({
-        format: "dd/mm/yyyy",
-        autoclose: true,
-        todayHighlight: true,
-        startDate: currentDate,
-        orientation: "bottom right"
-    });
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
 
-    $(".due-date-button").on("click", function (event) {
-        $(".due-date-button")
-            .datepicker("show")
-            .on("changeDate", function (dateChangeEvent) {
-                $(".due-date-button").datepicker("hide");
-                $(".due-date-label").text(formatDate(dateChangeEvent.date));
-            });
+        // Ball collision with walls
+        if (this.size + this.x > canvas.width || this.x - this.size < 0) {
+            this.speedX = -this.speedX;
+        }
+
+        if (this.size + this.y > canvas.height || this.y - this.size < 0) {
+            this.speedY = -this.speedY;
+        }
+
+        this.draw();
+    }
+}
+
+// Create ball at mouse/tap position
+canvas.addEventListener('click', (e) => {
+    const posX = e.clientX;
+    const posY = e.clientY;
+    balls.push(new Ball(posX, posY));
+});
+
+// Update canvas
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    balls.forEach(ball => {
+        ball.update();
     });
-};
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+// Handle window resizing
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});

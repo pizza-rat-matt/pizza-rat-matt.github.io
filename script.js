b3f2f1ad73b12ae228c1aee7f0631769cb35b450
Matt Christian <matthew.bryan.christian@gmail.com>
2023-08-10 19:47:22 -0400

    no for loop for balls

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ class Ball {
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fillStyle = this.color;
         ctx.fillText(this.name, this.x, this.y);
+        console.log('I drew')
         ctx.fill();
     }
 

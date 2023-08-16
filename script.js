690a7712947a3fd260f60850e1a8fa2fece41636
Matt Christian <matthew.bryan.christian@gmail.com>
2023-08-16 14:42:46 -0400

    what is the difference between svg and canvas

--- script.js
+++ script.js
@@ -144,7 +144,7 @@ const data = {
     ]
 };
 
-const svg = d3.select("canvas");
+const svg = d3.select("svg");
 console.log(svg)
 const width = +svg.attr("width");
 const height = +svg.attr("height");
